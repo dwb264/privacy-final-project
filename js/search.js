e49124c2c471fb7cac41c4b546090214add5407f
Devon Bain <dwb264@cornell.edu>
2018-05-12 00:33:06 -0400

    fixed chart and added more dates

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
-var network, fulltext;
+var network, fulltext, textByYear;
 var keywords = [];
-var chart;
+var chart, years;
 
 d3.queue()
 .defer(d3.json, "data/network.json")
@@ -9,39 +9,26 @@ d3.queue()
 
 function show_viz(error, net, text) {
 	network = net;
+
 	fulltext = text;
 	fulltext.forEach(function(d) {
 		d.text = d.text.split(". ");
 	})
 
-	var years = ['x'];
-	for (var i = 1999; i < 2019; i++) {
-		years.push(String(i));
-	}
+	textByYear = d3.nest()
+  	.key(function(d) { 
+  		var a = d.paper.split(" ")
+  		return parseInt(a[a.length - 1]); 
+  	})
+  	.rollup(function(v) { 
+  		return v;
+  	})
+  	.object(text);
 
-	chart = c3.generate({
-		bindto: "#timeseries",
-	    data: {
-	        x: 'x',
-	        xFormat: '%Y',
-	        columns: [
-	            years,
-	            ['Count of Papers', 1,0,0,1,1,1,3,1,4,1,4,7,7,10,5,10,7,8,0,1]
-	        ]
-	    },
-	    axis: {
-	        x: {
-	            type: 'timeseries',
-	            tick: {
-	                format: '%Y',
-	            }
-	        },
-	        y: {
-	        	min: 0,
-	        	padding: { bottom: 0 }
-	        }
-	    }
-	});
+	years = ['x'];
+	for (var i = 1999; i < 2019; i++) {
+		years.push("01-01-"+String(i));
+	}
 }
 
 d3.select("#search").on("change", function() {
@@ -55,17 +42,37 @@ d3.select("#search").on("change", function() {
 		return 0;
 	});
 
-	var data = [keyword];
-	for (var i = 1999; i < 2019; i++) {
-		var r = results.filter(function(d) { return d.year == i});
-		if (!(r)) {
-			data.push(0);
-		} else {
-			data.push(d3.sum(r, function(d) { return d.count }));
-		}
-	}
+	var data = find_normalized(keyword);
 
-	chart.load({columns: [data] });
+	if (!(chart)) {
+		chart = c3.generate({
+			bindto: "#timeseries",
+		    data: {
+		        x: 'x',
+		        xFormat: '%d-%m-%Y',
+		        columns: [
+		            years,
+		            data
+		        ]
+		    },
+		    axis: {
+		        x: {
+		            type: 'timeseries',
+		            tick: {
+		                format: '%Y',
+		            }
+		        },
+		        y: {
+		        	label: "% of Total Words",
+		        	min: 0,
+		        	padding: { bottom: 0 }
+		        }
+		    }
+		});
+
+	} else {
+		chart.load({columns: [data] });
+	}
 
 	var tag = d3.select("#keywords")
 		.append("div");
@@ -112,3 +119,24 @@ function find(keyword) {
 	return results;
 }
 
+function find_normalized(keyword) {
+	results = [keyword];
+
+	for (var i = 1999; i < 2019; i++) {
+		if (textByYear[i]) {
+			var keyword_count = 0;
+			var total_wordcount = 0;
+			textByYear[i].forEach(function(f) {
+				var txt = f.text.join(" ").toLowerCase();
+				var re = new RegExp(keyword.toLowerCase(), 'g');
+				if (txt.match(re)) keyword_count += txt.match(re).length * keyword.split(" ").length;
+				total_wordcount += txt.split(" ").length;
+			});
+			results.push(parseFloat(keyword_count)/total_wordcount);
+		} else {
+			results.push(0);
+		}
+	}
+
+	return results;
+}
